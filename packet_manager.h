c07ef700fd2b43b6d0821d2173a51f02a88eeb4f
likeyiyy <929812468@qq.com>
2014-06-18 15:46:03 +0800

    what fuck that

--- packet_manager.h
+++ packet_manager.h
@@ -18,11 +18,11 @@ typedef struct _session_manager
 {
     pthread_t          id;
     int                index;
-    int                list_length;
-    pthread_mutex_t    list_lock;
-    struct list_head * list;
-    pool_t           * session_pool;
-    session_queue_t  * queue;
+    //int                list_length;
+    //pthread_mutex_t    list_lock;
+    struct hash_table * ht;
+    pool_t            * session_pool;
+    session_queue_t   * queue;
 }manager_t;
 typedef struct _set
 {
@@ -37,4 +37,7 @@ manager_set_t * init_manager_set(uint32_t length);
 * 真正的工作者。
 * */
 void * packet_manager_loop(void * arg);
+
+struct blist * find_list(struct list_head * head, session_item_t * session);
+void delete_session(hash_table * ht,struct list_head * list);
 #endif
